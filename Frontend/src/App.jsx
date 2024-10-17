a9ce600e944a26debbb53792246e6d4b15fe973e
Harsh Ninave <harshninave58@gmail.com>
2024-10-18 01:56:24 +0530

    Frontend initialized!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Ride Sharing!</h1>
+    </div>
+  );
+}
