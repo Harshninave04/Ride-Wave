02f65ab55969f2744a1833ac913a1605bc010aff
Harsh Ninave <harshninave58@gmail.com>
2024-10-20 21:09:30 +0530

    Unauthorized user can't access protected routes!

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import Homepage from '../pages/prelogin/Homepage';
 import Login from '../pages/prelogin/Login';
 import Signup from '../pages/prelogin/Signup';
+import ProtectedRoute from './ProtectedRoute';// Import ProtectedRoute
 
 const AppRoutes = () => {
   return (
@@ -12,6 +13,10 @@ const AppRoutes = () => {
       <Route path="/" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/dashboard"
+        element={<ProtectedRoute>{/* Your Protected Component */}</ProtectedRoute>}
+      />
     </Routes>
   );
 };
